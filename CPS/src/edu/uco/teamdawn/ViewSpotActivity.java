7562d6883d852d2d057036d1cfe5347cdd99ef80
Dawn <moore.dawn12@gmail.com>
2012-11-06 17:06:23 -0600

    Dawn added her key.

--- CPS/src/edu/uco/teamdawn/ViewSpotActivity.java
+++ CPS/src/edu/uco/teamdawn/ViewSpotActivity.java
@@ -1,7 +1,13 @@
 package edu.uco.teamdawn;
 
+import edu.uco.dmoore35.tp.AddCourse;
+import edu.uco.dmoore35.tp.CoursesActivity;
+import edu.uco.dmoore35.tp.R;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,7 +35,27 @@ public class ViewSpotActivity extends FragmentActivity {
 		//intent.putExtra("spotActivity", true);
 		//startActivity(intent);
 	}
-	
+	 	@Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	    	menu.add(1, 1, 0,"Add a Course");
+	        getMenuInflater().inflate(R.menu.activity_courses, menu);
+	        return true;
+	    }
+
+		@Override
+		public boolean onMenuItemSelected(int featureId, MenuItem item) {
+			// TODO Auto-generated method stub
+			switch(item.getItemId()){
+			case 1:
+				//add a course
+				Intent addCourse = new Intent(CoursesActivity.this, AddCourse.class);
+				startActivity(addCourse);
+				break;
+			}
+			return super.onMenuItemSelected(featureId, item);
+		
+		}
+
 	
 	
 }
